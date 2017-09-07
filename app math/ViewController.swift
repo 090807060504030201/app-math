24c09f8e7bd6fc6fa11f6f9b8f68dead4aeca2c9
M.F.T <muhammadfauzantaufikkurrahman@Haryounous-MacBook-Pro.local>
2017-09-07 14:01:50 +0700

    Initial Commit

---
+++ app math/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app math
+//
+//  Created by M.F.T on 9/7/17.
+//  Copyright © 2017 M.F.T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
